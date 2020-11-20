96b5dc0277398e68741bb64dbfd3004de5db21d9
Piotr Ste <pstefanski@gmail.com>
2020-11-20 23:12:06 +0100

    once again the same
    users activites, sign in sign up new app look

--- app/src/main/java/com/example/stagemanager/GoogleSheetsHandler.java
+++ app/src/main/java/com/example/stagemanager/GoogleSheetsHandler.java
@@ -1,4 +1,67 @@
 package com.example.stagemanager;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.ValueRange;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+
 public class GoogleSheetsHandler {
+
+    public static void init() throws IOException, GeneralSecurityException {
+        // The ID of the spreadsheet to retrieve data from.
+        String spreadsheetId = "my-spreadsheet-id"; // TODO: Update placeholder value.
+
+        // The A1 notation of the values to retrieve.
+        String range = "my-range"; // TODO: Update placeholder value.
+
+        // How values should be represented in the output.
+        // The default render option is ValueRenderOption.FORMATTED_VALUE.
+        String valueRenderOption = ""; // TODO: Update placeholder value.
+
+        // How dates, times, and durations should be represented in the output.
+        // This is ignored if value_render_option is
+        // FORMATTED_VALUE.
+        // The default dateTime render option is [DateTimeRenderOption.SERIAL_NUMBER].
+        String dateTimeRenderOption = ""; // TODO: Update placeholder value.
+
+        Sheets sheetsService = createSheetsService();
+        Sheets.Spreadsheets.Values.Get request =
+                sheetsService.spreadsheets().values().get(spreadsheetId, range);
+        request.setValueRenderOption(valueRenderOption);
+        request.setDateTimeRenderOption(dateTimeRenderOption);
+
+        ValueRange response = request.execute();
+
+        // TODO: Change code below to process the `response` object:
+        System.out.println(response);
+    }
+
+    public static Sheets createSheetsService() throws IOException, GeneralSecurityException {
+        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+
+        // TODO: Change placeholder below to generate authentication credentials. See
+        // https://developers.google.com/sheets/quickstart/java#step_3_set_up_the_sample
+        //
+        // Authorize using one of the following scopes:
+        //   "https://www.googleapis.com/auth/drive"
+        //   "https://www.googleapis.com/auth/drive.file"
+        //   "https://www.googleapis.com/auth/drive.readonly"
+        //   "https://www.googleapis.com/auth/spreadsheets"
+        //   "https://www.googleapis.com/auth/spreadsheets.readonly"
+        GoogleCredential credential = null;
+
+        return new Sheets.Builder(httpTransport, jsonFactory, credential)
+                .setApplicationName("Google-SheetsSample/0.1")
+                .build();
+    }
 }
+
+
--- app/src/main/java/com/example/stagemanager/stageCrew/StageCrewMainActivity.java
+++ app/src/main/java/com/example/stagemanager/stageCrew/StageCrewMainActivity.java
@@ -1,14 +1,58 @@
-package com.example.stagemanager;
+package com.example.stagemanager.stageCrew;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.stagemanager.LoginActivity;
+import com.example.stagemanager.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class StageCrewMainActivity extends AppCompatActivity {
+
+    private FloatingActionButton stagefab;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stage_crew_main2);
+        setContentView(R.layout.activity_stage_crew_main);
+
+        linkResourcesToFields();
+        firebaseInit();
+        floatingButtonListener();
+
+    }
+
+    void floatingButtonListener(){
+        FloatingActionButton fab = findViewById(R.id.stagefab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fAuth.signOut();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            }
+        });
+    }
+
+    void getFromGoogle() {
+
+
+    }
+
+    ////////////// init
+    void linkResourcesToFields(){
+        stagefab = findViewById(R.id.stagefab);
+    }
+
+    void firebaseInit() {
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
     }
 }
