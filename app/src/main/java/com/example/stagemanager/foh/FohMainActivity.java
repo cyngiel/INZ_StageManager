6b05796dd7d5b1d6deaecc3dcfb803f181a2d745
Piotr Ste <pstefanski@gmail.com>
2020-11-20 19:47:30 +0100

    working signup/sign iN
    activietes for admin stage and foh. app looks pretty now

---
+++ app/src/main/java/com/example/stagemanager/foh/FohMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.stagemanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FohMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foh_main);
+    }
+}
---
+++ app/src/main/java/com/example/stagemanager/stageCrew/StageCrewMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.stagemanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StageCrewMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stage_crew_main2);
+    }
+}
