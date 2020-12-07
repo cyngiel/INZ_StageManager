50b089a1b7c06160a8071b2efb779b08242b2682
Piotr Ste <pstefanski@gmail.com>
2020-12-07 14:44:06 +0100

    timer working ceo

--- app/src/main/java/com/example/stagemanager/stageCrewCeo/StageCrewCeoMainActivity.java
+++ app/src/main/java/com/example/stagemanager/stageCrewCeo/StageCrewCeoMainActivity.java
@@ -2,8 +2,10 @@ package com.example.stagemanager.stageCrewCeo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +14,6 @@ import com.example.stagemanager.DisplaySheetActivity;
 import com.example.stagemanager.GlobalValues;
 import com.example.stagemanager.LoginActivity;
 import com.example.stagemanager.R;
-import com.example.stagemanager.admin.AdminMainActivity;
 import com.example.stagemanager.firebaseMessaging.CustomMessageActivity;
 import com.example.stagemanager.firebaseMessaging.FCMonClickListenerSender;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -27,7 +28,10 @@ public class StageCrewCeoMainActivity extends AppCompatActivity {
 
     private FloatingActionButton stagefab;
 
-    Button ceoConfirmStageBtn, ceoConfirmStageAllBtn, ceoNextStageBtn, ceoAbortStageBtn, ceoAbortStageAllBtn, ceoCustomMsgBtn, ceoDispSheet;
+    Button ceoConfirmStageBtn, ceoConfirmStageAllBtn, ceoNextStageBtn, ceoAbortStageBtn, ceoAbortStageAllBtn, ceoCustomMsgBtn, ceoDispSheet, ceoTimerBtn;
+    Chronometer ceoChronometer;
+
+    boolean isCounting;
 
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
@@ -50,7 +54,7 @@ public class StageCrewCeoMainActivity extends AppCompatActivity {
         ceoAbortStageAllBtn.setVisibility(View.GONE);
         ceoConfirmStageBtn.setVisibility(View.GONE);
         ceoAbortStageBtn.setVisibility(View.GONE);
-        ceoNextStageBtn.setVisibility(View.GONE);
+        //ceoNextStageBtn.setVisibility(View.GONE);
 
         final DocumentReference df = fStore.collection("Users").document(fAuth.getUid());
         new Thread(new Runnable() {
@@ -59,7 +63,7 @@ public class StageCrewCeoMainActivity extends AppCompatActivity {
                 df.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                    String name = task.getResult().getString(GlobalValues.fs_fieldName);
+                        String name = task.getResult().getString(GlobalValues.fs_fieldName);
 
                         ceoConfirmStageAllBtn.setOnClickListener(new FCMonClickListenerSender("STAGE CREW READY", "from- " + name, getApplicationContext(), GlobalValues.userLvlFohProdCode));
                         ceoConfirmStageAllBtn.setVisibility(View.VISIBLE);
@@ -93,8 +97,36 @@ public class StageCrewCeoMainActivity extends AppCompatActivity {
             }
         });
 
+        ceoTimerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (isCounting) {
+                    ceoChronometer.stop();
+                    ceoTimerBtn.setText("start timer");
+                    isCounting = false;
+
+                } else {
+                    ceoChronometer.setBase(SystemClock.elapsedRealtime());
+                    ceoChronometer.start();
+                    ceoTimerBtn.setText("stop timer");
+                    isCounting = true;
+                }
+
+
+            }
+        });
     }
 
+    /////// timer
+
+    public void updateTimer() {
+
+    }
+
+    void initTimer() {
+
+    }
 
     ////////////// init
     void linkResourcesToFields() {
@@ -106,6 +138,8 @@ public class StageCrewCeoMainActivity extends AppCompatActivity {
         ceoConfirmStageBtn = findViewById(R.id.ceoConfirmStageBtn);
         ceoCustomMsgBtn = findViewById(R.id.ceoCustomMsgBtn);
         ceoDispSheet = findViewById(R.id.ceoDispSheet);
+        ceoTimerBtn = findViewById(R.id.ceoTimerBtn);
+        ceoChronometer = findViewById(R.id.ceoChronometer);
 //        stageNotifyTestBtn = findViewById(R.id.stageNotifyTestBtn);
     }
 
